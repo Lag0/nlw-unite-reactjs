c8ad8da9880965b9611197c08e26474c7a1aad3c
Lag0 <brunolago@incorppar.com>
2024-04-07 14:28:37 -0300

    feat: href for header logo, removing href for navlinks

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -4,10 +4,12 @@ import nlwIcon from "../assets/nlw-unite-icon.svg";
 export default function Header() {
   return (
     <header className="flex items-center gap-5 py-2">
-      <img src={nlwIcon} alt="NLW Unite" />
+      <a href="/">
+        <img src={nlwIcon} alt="NLW Unite" />
+      </a>
       <nav className="flex items-center gap-5">
-        <NavLink href="/eventos">Eventos</NavLink>
-        <NavLink href="/participantes">Participantes</NavLink>
+        <NavLink href="/">Eventos</NavLink>
+        <NavLink href="/">Participantes</NavLink>
       </nav>
     </header>
   );
