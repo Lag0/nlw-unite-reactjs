92bc729b28b3f3ac858e4dc22941b77e42324f24
Lag0 <brunolago@incorppar.com>
2024-04-08 17:36:28 -0300

    feat: attendees / total display

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
       </a>
       <nav className="flex items-center gap-5">
         <NavLink href="/eventos">Eventos</NavLink>
-        <NavLink href="/">Participantes</NavLink>
+        {/* <NavLink href="/">Participantes</NavLink> */}
       </nav>
     </header>
   );
